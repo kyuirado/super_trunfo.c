087c273a855803bd762c12b93eefd63fcbd8992c
SAMUEL <115481428+kyuirado@users.noreply.github.com>
2025-05-19 13:56:05 +0000

    Tarefa super_trunfo

--- super_trunfo.c
+++ super_trunfo.c
@@ -1,34 +1,37 @@
 //SUPER TRUNFO DE CARTAS.
+// char: Armazena um único caractere (letras, números, símbolos).
+// int: Armazena números inteiros (sem casas decimais).
+// float: Armazena números de ponto flutuante (com casas decimais) com precisão simples.
 
 #include <stdio.h>
-
+// Declaração das variáveis para a primeira carta.
 int main() {
-    // Declaração das variáveis para a primeira carta
-    int populacao1;
+
+    int populacao1; 
     int turisticos1;
     float area1;
     float pib1;
     char estado1;
-    char codigo1[2];
+    char cod[4];
     char cidade1[20];
-
-    // Declaração das variáveis para a segunda carta
+    
+ //Declaração das variáveis para a segunda carta
     int populacao2;
     int turisticos2;
     float area2;
     float pib2;
     char estado2;
-    char codigo2[2];
+    char codigo2[4];
     char cidade2[20];
     
 
-    // Entrada de dados da carta 01
+// Entrada de dados da carta 01
     printf("=== Cadastro da Carta 01 ===\n");
     printf("Estado (letra de A a H): ");
     scanf(" %c", &estado1);// Leitura de char, apenas uma letra (ex(A)).
 
     printf("Código da carta (ex: A01): ");
-    scanf("%s", codigo1);// Leitura de char até dois caracteres,(ex(01)).
+    scanf("%s", cod);// Leitura de char até dois caracteres,(ex(01)).
 
     printf("Nome da cidade: ");
     scanf("%s", cidade1); // Leitura de char tipo tring (ex(Salvador)).
@@ -51,10 +54,10 @@ int main() {
     scanf(" %c", &estado2);// Leitura de char, apenas uma letra (ex(B)).
 
     printf("Código da carta (ex: B03): ");
-    scanf("%s", codigo2);// Leitura de char até dois caracteres,(ex(02)).
+    scanf("%s", codigo2);// Leitura de char até dois caracteres,(ex(B02)).
 
     printf("Nome da cidade: ");
-    scanf("%s", cidade2);// Leitura de char tipo tring (ex(Salvador))
+    scanf("%s", cidade2);// Leitura de char tipo tring (ex(Salvador)).
 
     printf("População: ");
     scanf("%d", &populacao2);// Leitura de dados do tipo inteiro (ex(100))
@@ -69,18 +72,18 @@ int main() {
     scanf("%d", &turisticos2);// // Leitura de dados do tipo inteiro (ex(100)).
 
 //SAIDA DE DADOS DAS CARTAS
-    //Primeira carta    
+
+    //Primeira carta - 01 
     printf("\n=== Carta 01 ===\n");//Escrevendo dados da carta 01.
     printf("Estado: %c\n", estado1);//Escrevendo char, apenas uma letra. (ex(A)).
-    printf("Código: %s\n", codigo1);////Escrevendo char, até 2 números (ex(02)).
+    printf("Código: %s\n", cod);
     printf("Nome da Cidade: %s\n", cidade1);//Escrevendo char, tipo sting. (ex(SALVADOR)).
     printf("População: %d\n", populacao1); //Escrevendo int, número inteiro. (ex(100)).
     printf("Área: %.2f km²\n", area1);//Escrevendo float, ponto flutuante. (ex(1,000)).
     printf("PIB: %.2f bilhões de reais\n", pib1);//Escrevendo float, ponto flutuante. (ex(1,000)).
     printf("Número de Pontos Turísticos: %d\n", turisticos1);//Escrevendo int, número inteiro. (ex(10)).
 
-
-    //Segunda carta
+    //Segunda carta - 02
     printf("\n=== Carta 02 ===\n");//Escrevendo dados da carta 02.
     printf("Estado: %c\n", estado2);//Escrevendo char, apenas uma letra. (ex(B)).
     printf("Código: %s\n", codigo2);//Escrevendo char, até 2 números (ex(02)).
@@ -89,6 +92,16 @@ int main() {
     printf("Área: %.2f km²\n", area2);//Escrevendo float, ponto flutuante. (ex(1,000)).
     printf("PIB: %.2f bilhões de reais\n", pib2);//Escrevendo float, ponto flutuante. (ex(1,000)).
     printf("Número de Pontos Turísticos: %d\n", turisticos2);//Escrevendo int, número inteiro. (ex(10)).
+
+
+// LISTA DOS PRINCIPAIS ESPECIFICADORES.
+
+//%d: Imprime um inteiro no formato decimal.
+//%i: Equivalente a %d.
+//%f: Imprime um número de ponto flutuante no formato padrão.
+//%e: Imprime um número de ponto flutuante na notação científica.
+//%c: Imprime um único caractere.
+//%s: Imprime uma cadeia (string) de caracteres.
 
     return 0;
 }
